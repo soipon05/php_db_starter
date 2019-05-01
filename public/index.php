50c7d538a170a5520f2548e51ba5fb53bc505587
JUN <soipon0@gmail.com>
2019-05-01 21:35:17 +0900

    PDOの設定_DBの定義とエラーキャッチの処理

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo 'Hello World!';
+define('DB_DATABASE', 'dotinstall_db');
+define('DB_USERNAME', 'dbuser');
+define('DB_PASSWORD', 'password');
+define('PDO_DSN', 'mysql:dbhost=localhost;dbname=', DB_DATABASE);
+
+try {
+    // connect
+    $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    exit;
+}

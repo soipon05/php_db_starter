e84189bf1d2a16a37749f4b1e3f79414f2697678
JUN <soipon0@gmail.com>
2019-05-02 15:40:24 +0900

    名前付きパラメータの渡す方法

--- public/index.php
+++ public/index.php
@@ -3,16 +3,23 @@
 define('DB_DATABASE', 'dotinstall_db');
 define('DB_USERNAME', 'dbuser');
 define('DB_PASSWORD', 'password');
-define('PDO_DSN', 'mysql:host=localhost;dbname=', DB_DATABASE);
+define('PDO_DSN', 'mysql:dbhost=localhost;dbname=' . DB_DATABASE);
 
 try {
     // connect
     $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // insert
-    $db->exec("insert into users (name, score) values ('taguchi', 55)");
-    echo "user added";
+// 1. exec(): 結果を返さない、安全なSQL
+// 2. query(): 結果を返す、安全な、何回も実行されないSQL
+// 3. prepare(): 結果を返す、安全対策が必要、複数回実行されるSQL
+
+    // $stmt = $db->prepare("insert into users (name, score) values (?, ?)");
+    // $stmt->execute(['taguchi', 44]);
+    $stmt = $db->prepare("insert into users (name, score) values (:name, :score)");
+    $stmt->execute([':name'=>'fkoji', 'score'=>94]);
+    echo "inserted: " . $db->lastInsertId();
+
 
     // disconnect
     $db = null;

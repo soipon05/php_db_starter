7cb91091ba04683194645de8e69ae31ae11b39d3
JUN <soipon0@gmail.com>
2019-05-02 10:07:19 +0900

    exec()を使用

--- public/index.php
+++ public/index.php
@@ -3,12 +3,20 @@
 define('DB_DATABASE', 'dotinstall_db');
 define('DB_USERNAME', 'dbuser');
 define('DB_PASSWORD', 'password');
-define('PDO_DSN', 'mysql:dbhost=localhost;dbname=', DB_DATABASE);
+define('PDO_DSN', 'mysql:host=localhost;dbname=', DB_DATABASE);
 
 try {
     // connect
     $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // insert
+    $db->exec("insert into users (name, score) values ('taguchi', 55)");
+    echo "user added";
+
+    // disconnect
+    $db = null;
+
 } catch (PDOException $e) {
     echo $e->getMessage();
     exit;

3a37e6702399f11a914d7b9911181874b730d285
JUN <soipon0@gmail.com>
2019-05-02 16:02:52 +0900

    bindParam()を使って変数をbindする

--- public/index.php
+++ public/index.php
@@ -11,8 +11,8 @@ try {
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // 1. exec(): 結果を返さない、安全なSQL
-// 2. query(): 結果を返す、安全な、何回も実行されないSQL
-// 3. prepare(): 結果を返す、安全対策が必要、複数回実行されるSQL
+// bindValue: 値をバインドする
+// bindParam: 変数への参照をbind
 
     // $stmt = $db->prepare("insert into users (name, score) values (?, ?)");
     // $stmt->execute(['taguchi', 44]);
@@ -23,18 +23,20 @@ try {
 
     $name = 'taguchi';
     $stmt->bindValue(1, $name, PDO::PARAM_STR);
-    // 名前付きパラメータのときは第一引数に同じのを指定する
-    // $stmt->bindValue(':name', $name, PDO::PARAM_STR);
-    $score = 23;
-    $stmt->bindValue(2, $score, PDO::PARAM_INT);
+    // // 名前付きパラメータのときは第一引数に同じのを指定する
+    // // $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+    // $score = 23;
+    // $stmt->bindValue(2, $score, PDO::PARAM_INT);
+    // $stmt->execute();
+    // $score = 44;
+    // $stmt->bindValue(2, $score, PDO::PARAM_INT);
+    // $stmt->execute();
+
+    $stmt->bindParam(2, $score, PDO::PARAM_INT);
+    $score = 52;
     $stmt->execute();
-
-    $score = 44;
-    $stmt->bindValue(2, $score, PDO::PARAM_INT);
+    $score = 33;
     $stmt->execute();
-
-    // 文字列の時はPDO::PARAM＿STR、数値はPDO::PARAM_INT、NULLはPDO::PARAM_NULL、論理値はPDO::PARAM_BOOLと書く
-
 
 
     // disconnect

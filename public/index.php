94185413396ce22d52e1ad463024a51d121fe84f
JUN <soipon0@gmail.com>
2019-05-02 16:22:04 +0900

    whereやlimitを使った条件付き抽出

--- public/index.php
+++ public/index.php
@@ -10,8 +10,19 @@ try {
     $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // select all
-    $stmt = $db->query("select * from users");
+    // select
+    // $stmt = $db->query("select * from users");
+    // $stmt = $db->prepare("select score from users where score > ?");
+
+    // nameにtを含む人を探す条件分を書く、likeの指定はexecute(この中ですること)
+    // $stmt = $db->prepare("select name from users where name like > ?");
+    // $stmt->execute(['%t%']);
+
+    // executeの値は基本的に文字列で返されるのでlimitの後の？はそこをうまく読み取ってくれないのでbindValueを使う
+    $stmt = $db->prepare("select score from users order by score desc limit ?");
+    $stmt->bidnValue(1, 1, PDO::PARAM_INT);
+    $stmt->execute();
+
     $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
     foreach ($users as $user) {
         var_dump($user);

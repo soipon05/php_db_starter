9a20b0f322dd26ee736c533073dfbe08f3c9b16d
JUN <soipon0@gmail.com>
2019-05-02 16:33:20 +0900

    updateとdelete

--- public/index.php
+++ public/index.php
@@ -5,30 +5,34 @@ define('DB_USERNAME', 'dbuser');
 define('DB_PASSWORD', 'password');
 define('PDO_DSN', 'mysql:dbhost=localhost;dbname=' . DB_DATABASE);
 
+class User {
+    public $id;
+    public $name;
+    public $score;
+    public function show() {
+        echo "$this->name ($this->score)";
+    }
+}
+
 try {
     // connect
     $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // select
-    // $stmt = $db->query("select * from users");
-    // $stmt = $db->prepare("select score from users where score > ?");
+    // update
+    $stmt = $db->prepare("update users set score = :score where name = :name");
+    $stmt->execute([
+        ':score' => 100,
+        ':name'  => 'taguchi'
+    ]);
+    echo 'row updated: ' . $stmt->rowCount();
 
-    // nameにtを含む人を探す条件分を書く、likeの指定はexecute(この中ですること)
-    // $stmt = $db->prepare("select name from users where name like > ?");
-    // $stmt->execute(['%t%']);
-
-    // executeの値は基本的に文字列で返されるのでlimitの後の？はそこをうまく読み取ってくれないのでbindValueを使う
-    $stmt = $db->prepare("select score from users order by score desc limit ?");
-    $stmt->bidnValue(1, 1, PDO::PARAM_INT);
-    $stmt->execute();
-
-    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($users as $user) {
-        var_dump($user);
-    }
-
-    $stmt->rowCount() . "records found.";
+    // delete
+    $stmt = $db->prepare("delete from users where name = :name");
+    $stmt->execute([
+        ':name' => 'dotinstall'
+    ]);
+    echo 'row deleted: ' . $stmt->rowCount();
 
 
     // disconnect
